84ea5da1a16a15364cd18691abb081b300a2ee6c
Christina Liberto <cml53035@uga.edu>
2019-12-05 14:51:39 -0500

    finished controller and form

--- src/main/java/cs1302/arcade/Controller.java
+++ src/main/java/cs1302/arcade/Controller.java
@@ -11,6 +11,7 @@ public class Controller {
     public static int [][] GRID = Tetris.GRID;
 
     public static void MoveRight(Form form) {
+
 	if (form.a.getX() + MOVE <= XMAX - SIZE && form.b.getX() + MOVE <= XMAX - SIZE && form.c.getX() + MOVE <= XMAX - SIZE && form.d.getX() + MOVE <= XMAX - SIZE) {
 	    int movea = GRID[((int) form.a.getX() / SIZE) + 1][((int) form.a.getY() / SIZE)];
 	    int	moveb =	GRID[((int) form.b.getX() / SIZE) + 1][((int) form.b.getY() / SIZE)];
@@ -39,6 +40,79 @@ public class Controller {
             } //if
 	} //if                                                                                                                                                    
     } //MoveLeft
-    
+
+    public static Form makeRect () {
+
+	int block = (int)(Math.random() * 100);
+	String name;
+
+	Rectangle a = new Rectangle(SIZE - 1, SIZE - 1),
+	    b = new Rectangle(SIZE - 1, SIZE - 1),
+	    c = new Rectangle(SIZE - 1, SIZE - 1),
+	    d = new Rectangle(SIZE -1, SIZE - 1);  
+
+	if (block < 15) {
+	    a.setX(XMAX / 2 - SIZE);
+	    b.setX(XMAX / 2 - SIZE);
+	    b.setY(SIZE);
+	    c.setX(XMAX / 2);
+	    c.setY(SIZE);
+	    d.setX(XMAX / 2 + SIZE);
+	    d.setY(SIZE);
+	    name = "j";
+	} else if (block < 30) {
+	    a.setX(XMAX / 2 + SIZE);
+	    b.setX(XMAX / 2 - SIZE);
+	    b.setY(SIZE);
+	    c.setX(XMAX / 2);
+	    c.setY(SIZE);
+	    d.setX(XMAX / 2 + SIZE);
+	    d.setY(SIZE);
+	    name = "l";
+	} else if (block < 45) {
+	    a.setX(XMAX / 2 - SIZE);
+	    b.setX(XMAX / 2);
+	    b.setX(XMAX / 2 - SIZE);
+	    c.setY(SIZE);
+	    d.setX(XMAX / 2);
+	    d.setY(SIZE);
+	    name = "o";
+	} else if (block < 60) {
+	    a.setX(XMAX / 2 + SIZE);
+	    b.setX(XMAX / 2);
+	    b.setX(XMAX / 2);
+	    c.setY(SIZE);
+	    d.setX(XMAX / 2 - SIZE);
+	    d.setY(SIZE);
+	    name = "s";
+	} else if (block < 75) {
+	    a.setX(XMAX / 2 - SIZE);
+	    b.setX(XMAX / 2);
+	    b.setX(XMAX / 2);
+	    c.setY(SIZE);
+	    d.setX(XMAX / 2 + SIZE);
+	    name = "t";
+	} else if (block < 90) {
+	    a.setX(XMAX / 2 + SIZE);
+	    b.setX(XMAX / 2);
+	    b.setX(XMAX / 2 + SIZE);
+	    c.setY(SIZE);
+	    d.setX(XMAX / 2 + SIZE + SIZE);
+	    d.setY(SIZE);
+	    name = "z";
+	} else {
+	    a.setX(XMAX / 2 - SIZE - SIZE);
+	    b.setX(XMAX / 2 - SIZE);
+	    b.setX(XMAX / 2);
+	    d.setX(XMAX / 2 + SIZE);
+	    name = "i";
+	}
+
+	return new Form (a, b, c, d, name);
+	
+    }
+	    
+	    
+				 
     
 } //Controller
