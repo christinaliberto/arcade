330b96c3e6e24c9bc46570637c2aa3a70f3cc1bf
Christina Liberto <cml53035@uga.edu>
2019-12-06 15:49:52 -0500

    updated:

--- src/main/java/cs1302/arcade/ArcadeApp.java
+++ src/main/java/cs1302/arcade/ArcadeApp.java
@@ -108,65 +108,9 @@ public class ArcadeApp extends Application {
     }
 
     public void playTetris() {
-<<<<<<< HEAD
 
-	Stage tetris = new Stage();
-        tetris.initModality(Modality.APPLICATION_MODAL);
-        HBox main = new HBox();
-	GridPane grid = new GridPane();
-        main.setPadding(new Insets(15, 12, 15, 12));
-        main.setSpacing(20);
-	Image rainbow = new Image("http://hd-wallpapers.download/free/blocks_rainbow_3d_graphics_background_76559_6000x4000-3000x2000.jpg", 500, 600, false, false);
-        BackgroundImage back = new BackgroundImage(rainbow, BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.DEFAULT, BackgroundSize.DEFAULT);
-        Background background = new Background(back);
-        main.setBackground(background);
-        VBox left = new VBox();
-        VBox right = new VBox();
-        HBox next = new HBox();
-        left.setSpacing(20);
-        next.setAlignment(Pos.CENTER);
-        next.setBackground(new Background(new BackgroundFill(Color.LIGHTYELLOW, CornerRadii.EMPTY, Insets.EMPTY)));
-        next.setStyle("-fx-padding: 5;" + "-fx-border-style: solid outside;"
-		      + "-fx-border-width: 5;" + "-fx-border-insets: 5;"
-		      + "-fx-border-radius: 5;" + "-fx-border-color: red;");
-        Text info = new Text();
-        info.setText("SCORE:\n" + " 0\n" + "LEVEL :\n" + " 1\n" + "LINES :\n" + " 0");
-        info.setFill(Color.RED);
-        info.setFont(Font.font("Futura", FontWeight.BOLD, 16));
-        next.getChildren().add(info);
-        HBox stats = new HBox();
-        ImageView logo = new ImageView(new Image("https://www2.instantticketcontest.com/TX/Tetris/images/logo-game@2x.png", 150, 150, false, false));
-        grid.setPrefSize(300, 600);
-        grid.setMaxSize(Region.USE_COMPUTED_SIZE, Region.USE_COMPUTED_SIZE); 
 
-	//adds cols and rows to grid
-        final int numCols = 10;
-	final int numRows = 20;
-        for (int i = 0; i < numCols; i++) {
-            ColumnConstraints colConst = new ColumnConstraints();
-            colConst.setPercentWidth(100.0 / numCols);  
-            grid.getColumnConstraints().add(colConst);
-        }
-        for (int i = 0; i < numRows; i++) {
-            RowConstraints rowConst = new RowConstraints();
-            rowConst.setPercentHeight(100.0 / numRows);
-            grid.getRowConstraints().add(rowConst);         
-        }
-        grid.setGridLinesVisible(true);
-	grid.setBackground(new Background(new BackgroundFill(Color.BLACK, CornerRadii.EMPTY, Insets.EMPTY)));
-        left.getChildren().addAll(logo, next, stats);
-        right.getChildren().addAll(grid);
-        main.getChildren().addAll(left, right);
-
-	Scene scene = new Scene(main);
-	tetris.setTitle("Tetris");
-	tetris.setWidth(500);
-	tetris.setHeight(560);
-	tetris.setScene(scene);
-        tetris.show();
-=======
-	Tetris tetris = new Tetris();
->>>>>>> 6515fb09926dfe24dfc0014591d1ba601df26432
+	    Tetris tetris = new Tetris();
     }
     
     public VBox createMenu() {
