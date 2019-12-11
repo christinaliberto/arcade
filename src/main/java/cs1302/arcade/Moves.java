1fb71a1aa88920f6bf1d8fa2a14db7814cf31a2d
Nicolette Molloy <nrm00226@uga.edu>
2019-12-11 14:53:01 -0500

    up

--- src/main/java/cs1302/arcade/Moves.java
+++ src/main/java/cs1302/arcade/Moves.java
@@ -192,7 +192,7 @@ public class Moves {
         }
 
         if (canMove) {
-            xint--;
+            xint++;
             for (Rectangle r : shape) {
                 if (r != null) {
                     col = GridPane.getColumnIndex(r);
